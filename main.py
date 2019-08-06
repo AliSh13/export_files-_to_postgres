938b21f5ba1d138e2f487b0593562a47c1e8936e
alex <alex-just2200@yandex.ru>
2019-08-06 23:16:45 +0300

    added export xlsx to sql

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def export_excel_to_sql(path_to_file, table_name, index, sheet=1,
         df.columns = [c.lower() for c in df.columns]
     if rename_column:
         df.rename(columns=rename_column, inplace=True)
-    # проверяем наличие таблицы и, если существует, проверяем новый файл на уникальность строк    
-    if exists_table(db_connect(), table_name) :
+    # проверяем наличие таблицы и, если существует, проверяем новый файл на уникальность строк
+    if exists_table(db_connect(), table_name):
         df = unique_lines_only(df, table_name, db_connect(), list(df.columns))
 
     df.to_sql(table_name, db_connect(), if_exists='append', index=index)
