63be91862897a3c65e05183e691cf1f694d12c6a
alex <alex-just2200@yandex.ru>
2019-08-06 23:04:25 +0300

    create main.py (основной функционал)

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sqlalchemy import types, create_engine
+from openpyxl import load_workbook
+from d6tstack import utils
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    main()
